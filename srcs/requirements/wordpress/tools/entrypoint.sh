cf81ae4306c59842c026b831822288199f73672f
MohamedBehhar <bahharmohamed18@gmail.com>
2023-01-26 15:28:31 +0100

    adding comments, before removing conf folder

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,51 +1,46 @@
 #!/bin/sh
 
-set -x
-
+# install wp cli 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+# install wp core to be able to setup the website using commands
 wp core download --allow-root
+# make FastCGI Process Manager to listen on port 9000
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir /run/php
 
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
-    echo "***********"
+    echo "wordpress already exist"
 fi
-
-
 
 # Copy the original WordPress config file
 cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 cd /var/www/wordpress/
 cat wp-config.php
 
-# sleep 5
-
-
 # Set the database details with the environment variables
-echo "************************"
-
 sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/wordpress/wp-config.php
 sed -i "s/username_here/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
 sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/wordpress/wp-config.php
 sed -i "s/localhost/$DB_HOST/g" /var/www/wordpress/wp-config.php
 
-echo "************************"
-
 wp config create
 
+# install wp and creat admin user 
 wp core install --url=https://localhost --title=Inception --admin_user=${ADMIN_USER} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=mbehhar@student.1337.ma --allow-root
+
+# creat normal user 
 wp user create ${MYSQL_USER} behharmohamed18@gmail.com --user_pass=${MYSQL_PASSWORD} --allow-root
 
+# allow comments on the website 
+wp option update comment_status open --allow-root
 
+# give nginx the permission to access wp files
 chown -R www-data:www-data /var/www/wordpress
-
-
-php-fpm7.3 -F
 
 exec "$@"
 
